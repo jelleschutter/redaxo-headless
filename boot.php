9d0f81e970cfd77e85fb2a7c7db5c078dedf928c
Jelle Schutter <jelle@schutter.xyz>
2021-01-20 00:04:55 +0100

    set default override output explicitly to false

--- boot.php
+++ boot.php
@@ -10,7 +10,7 @@
  * @var rex_addon $this
  */
 
-if (rex::isFrontend() && $this->getConfig('override_output') && !rex_get('rex-api-call')) {
+if (rex::isFrontend() && $this->getConfig('override_output', false) && !rex_get('rex-api-call')) {
     rex_extension::register('ART_INIT', function () {
         rex_response::sendContent($this->getConfig('spa_html', ''));
     });
