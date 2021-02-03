fccc9623fcc4a94ff1450453440b36d9ae4372e9
Jelle Schutter <jelle@schutter.xyz>
2021-02-04 00:10:43 +0100

    add deployment plugin

---
+++ plugins/deploy/lib/api/rex_api_headless_deploy.php
@@ -0,0 +1,77 @@
+<?php
+
+class rex_api_headless_deploy extends rex_api_function {
+
+    protected $published = true;
+
+    function execute()
+    {
+        $addon = rex_addon::get('headless');
+        $plugin = $addon->getPlugin('deploy');
+
+        if (rex_post('token', 'string', null) !== $plugin->getConfig('token', '')) {
+            rex_response::setStatus(403);
+            rex_response::sendJson([]);
+            exit;
+        }
+
+        $files = rex_files('file', 'array', null);
+
+        if ($files === null) {
+            rex_response::setStatus(400);
+            rex_response::sendJson([]);
+            exit;
+        }
+
+        $fileName = $files['tmp_name'];
+
+        $zip = new ZipArchive();
+        $zip->open($fileName);
+
+        $spaHtml = false;
+
+        for ($i = 0; $i < $zip->numFiles; $i++) {
+            $file = $zip->statIndex($i);
+            if ($file['name'] == 'index.html') {
+                $html = $zip->getFromIndex($i);
+
+                $dom = new DOMDocument();
+                libxml_use_internal_errors(true);
+                $dom->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'), LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+                libxml_clear_errors();
+
+
+                $scripts = $dom->getElementsByTagName('script');
+                foreach ($scripts as $script) {
+                    $src = $script->getAttribute('src');
+                    if (substr($src, 0, 1) === '/') {
+                        $script->setAttribute('src', $plugin->getAssetsUrl($src));
+                    }
+                }
+
+                $links = $dom->getElementsByTagName('link');
+                foreach ($links as $link) {
+                    $href = $link->getAttribute('href');
+                    if (substr($href, 0, 1) === '/') {
+                        $link->setAttribute('href', $plugin->getAssetsUrl($href));
+                    }
+                }
+
+                $spaHtml = $dom->saveHTML($dom->documentElement);
+            } else {
+                $newPath = $plugin->getAssetsPath($file['name']);
+                $newDir = pathinfo($newPath, PATHINFO_DIRNAME);
+                if (!is_dir($newDir)) {
+                    rex_dir::create($newDir);
+                }
+                file_put_contents($plugin->getAssetsPath($file['name']), $zip->getFromIndex($i));
+            }
+        }
+        $zip->close();
+
+        if ($spaHtml !== false) {
+            $addon->setConfig('spa_html', $spaHtml);
+        }
+        exit;
+    }
+}
---
+++ plugins/deploy/pages/index.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Headless Addon.
+ *
+ * @author jelle@schutter.xyz
+ *
+ * @package redaxo\headless\deploy
+ *
+ * @var rex_plugin $this
+ */
+
+$form = rex_config_form::factory($this->getName());
+
+$field = $form->addSelectField('enable_deploy', null, ['class' => 'form-control']);
+$field->setLabel($this->i18n('enable_deploy'));
+$select = new rex_select();
+$select->addOption('Enabled', true);
+$select->addOption('Disabled', false);
+$field->setSelect($select);
+
+$field = $form->addTextField('token', null, ['class' => 'form-control']);
+$field->setNotice(str_replace('{token}', bin2hex(random_bytes((32-(32%2))/2)), $this->i18n('token_note')));
+$field->setLabel($this->i18n('token'));
+$field->setAttribute('auto_complete', false);
+
+$fragment = new rex_fragment();
+$fragment->setVar('class', 'edit', false);
+$fragment->setVar('title', $this->i18n('config'), false);
+$fragment->setVar('body', $form->get() , false);
+echo $fragment->parse('core/page/section.php');
